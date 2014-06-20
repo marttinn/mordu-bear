211d67af05013e8f03de20b6c4df6a8fdc16351b
Martin Granados <margrangr@gmail.com>
2014-06-19 19:44:10 -0500

    Primer commitgit add . :D :D :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MorduBear::Application.config.secret_key_base = 'd36603f3fd2ba4b7f4da4ee928c7a685d05fdba801651420cc3a46a11c5bed53414f8dd0a7c8d62f9ee2fe1c21bbf488b66c088e71c0f25613302e0c1f5cdba4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MorduBear::Application.config.session_store :cookie_store, key: '_mordu-bear_session'
